c2791049899fce4d394bbd8616622b53c7198fc2
SarathkumarSM <sarathkumarsm2603@gmail.com>
2024-02-13 15:17:53 +0530

    models_assigned_azure_blob

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 linemodelpath='./src/utilities/primalinenet/anuvaad_line_v1.pth'
-url_line='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_line_v1.pth'
+# url_line='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_line_v1.pth'
+url_line = 'https://lekhaanuvaad.blob.core.windows.net/lekhaanuvaad-pubnet-weights/anuvaad_line_v1.pth'
 
 rm $linemodelpath
 if ! [ -f $linemodelpath ]; then
